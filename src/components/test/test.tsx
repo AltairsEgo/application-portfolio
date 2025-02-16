1c01a9afbaf9dc95390a796bfff89e978ad3a146
AltairsEgo <justins.9480@gmail.com>
2025-02-16 13:15:17 +0100

    feat: test commit.

--- src/components/test/test.tsx
+++ src/components/test/test.tsx
@@ -1,30 +1,28 @@
-import styles from './test.module.css'
+import styles from "./test.module.css"
 
-const Test = () =>
-{
-    const products = [
-        { id: 1, name: "Laptop", categories: ["Electronics", "Computers"] },
-        { id: 2, name: "Phone", categories: ["Electronics", "Mobile"] },
-        { id: 3, name: "Keyboard", categories: ["Computers", "Accessories"] }
-      ];
-    
-    const flattenProductsArray = [...new Set(products.map(p => p.categories).flat())]
+const Test = () => {
+  const products = [
+    { id: 1, name: "Laptop", categories: ["Electronics", "Computers"] },
+    { id: 2, name: "Phone", categories: ["Electronics", "Mobile"] },
+    { id: 3, name: "Keyboard", categories: ["Computers", "Accessories"] },
+  ]
 
+  const flattenProductsArray = [
+    ...new Set(products.map((p) => p.categories).flat()),
+  ]
 
+  console.log("test")
 
-    return (<div className={styles['test-wrapper']}>
-        <section>
+  return (
+    <div className={styles["test-wrapper"]}>
+      <section>
         <header>
-        <h2>
-            This is an testing area to try out stuff
-        </h2>
+          <h2>This is an testing area to try out stuff</h2>
         </header>
-        <p>
-            {flattenProductsArray + " "}
-            </p>
-    </section>
-        </div>
-    )
+        <p>{flattenProductsArray + " "}</p>
+      </section>
+    </div>
+  )
 }
 
-export {Test}
+export { Test }
